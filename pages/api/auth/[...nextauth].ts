fe20bacbfc3cbacbcf111ad62b923c2a3105bc6a
dtinth on MBP M1 <dtinth@spacet.me>
2023-06-22 20:52:17 +0700

    integrate Authgarten

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,38 +1,62 @@
 import NextAuth, { NextAuthOptions } from "next-auth"
-import GoogleProvider from "next-auth/providers/google"
-import FacebookProvider from "next-auth/providers/facebook"
-import GithubProvider from "next-auth/providers/github"
-import TwitterProvider from "next-auth/providers/twitter"
-import Auth0Provider from "next-auth/providers/auth0"
+import type { OAuthConfig } from "next-auth/providers"
+
+/**
+ * Data contained in ID token returned by Authgarten OIDC provider.
+ * https://github.com/creatorsgarten/creatorsgarten.org/blob/main/src/backend/auth/mintIdToken.ts
+ */
+export interface AuthgartenOidcClaims {
+  /** Unique user ID (24 characters hexadecimal) */
+  sub: string
+
+  /** Full name */
+  name: string
+
+  /** Avatar URL */
+  picture: string
+
+  /** Connections */
+  connections: {
+    github?: {
+      /** GitHub user ID */
+      id: number
+      /** Username */
+      username: string
+    }
+    eventpop: {
+      /** Eventpop user ID */
+      id: number
+    }
+  }
+}
+
+const creatorsgartenProvider = {
+  type: "oauth",
+  id: "creatorsgarten",
+  name: "Creatorsgarten",
+  issuer: "https://creatorsgarten.org",
+  wellKnown: "https://creatorsgarten.org/.well-known/openid-configuration",
+  authorization: { params: { response_type: "id_token" } },
+
+  // Add your Client ID to this URL to integrate with Authgarten:
+  // https://github.com/creatorsgarten/creatorsgarten.org/blob/main/src/constants/oauth.ts
+  clientId: "https://github.com/dtinth/authgarten-example",
+
+  idToken: true,
+  profile: (profile) => {
+    return {
+      id: profile.sub,
+      name: profile.name,
+      image: profile.picture,
+    }
+  },
+} satisfies OAuthConfig<AuthgartenOidcClaims>
 
 // For more information on each option (and a full list of options) go to
 // https://next-auth.js.org/configuration/options
 export const authOptions: NextAuthOptions = {
   // https://next-auth.js.org/configuration/providers/oauth
-  providers: [
-    Auth0Provider({
-      clientId: process.env.AUTH0_ID,
-      clientSecret: process.env.AUTH0_SECRET,
-      issuer: process.env.AUTH0_ISSUER,
-    }),
-    FacebookProvider({
-      clientId: process.env.FACEBOOK_ID,
-      clientSecret: process.env.FACEBOOK_SECRET,
-    }),
-    GithubProvider({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET,
-    }),
-    GoogleProvider({
-      clientId: process.env.GOOGLE_ID,
-      clientSecret: process.env.GOOGLE_SECRET,
-    }),
-    TwitterProvider({
-      clientId: process.env.TWITTER_ID,
-      clientSecret: process.env.TWITTER_SECRET,
-      version: "2.0",
-    }),
-  ],
+  providers: [creatorsgartenProvider],
   callbacks: {
     async jwt({ token }) {
       token.userRole = "admin"
